0072b2af8beb4c513445b727af91148fea15ea94
Masangkay-JohnAlbert <masangkayjohnalbert1216@gmail.com>
2026-09-02 10:16:57 +0800

    Add initial project files including HTML, TypeScript, and configuration

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+var MemberRole;
+(function (MemberRole) {
+    MemberRole[MemberRole["Developer"] = 0] = "Developer";
+    MemberRole[MemberRole["Designer"] = 1] = "Designer";
+    MemberRole[MemberRole["Tester"] = 2] = "Tester";
+    MemberRole[MemberRole["ProjectManager"] = 3] = "ProjectManager";
+    MemberRole[MemberRole["Documenter"] = 4] = "Documenter";
+})(MemberRole || (MemberRole = {}));
+function roleToString(role) {
+    return MemberRole[role];
+}
+const member1Object = {
+    id: 1,
+    name: "John Albert Masangkay",
+    role: MemberRole.Developer,
+    age: 22,
+    email: "MJD0701@dlsud.edu.ph",
+    isActive: true,
+    print() {
+        console.log("---- Member 1 ----");
+        console.log(`ID: ${this.id}`);
+        console.log(`Name: ${this.name}`);
+        console.log(`Role: ${roleToString(this.role)}`);
+        console.log(`Age: ${this.age}`);
+        console.log(`Email: ${this.email}`);
+        console.log(`Active: ${this.isActive}`);
+    },
+};
+member1Object.print();
---
+++ script.ts
@@ -0,0 +1,41 @@
+enum MemberRole {
+  Developer,
+  Designer,
+  Tester,
+  ProjectManager,
+  Documenter,
+}
+
+type TeamMember = {
+  id: number;
+  name: string;
+  role: MemberRole;
+  age: number;
+  email: string;
+  isActive?: boolean;
+  print: () => void;
+};
+
+function roleToString(role: MemberRole): string {
+  return MemberRole[role];
+}
+
+const member1Object: TeamMember = {
+  id: 1,
+  name: "John Albert Masangkay",
+  role: MemberRole.Developer,
+  age: 22,
+  email: "MJD0701@dlsud.edu.ph",
+  isActive: true,
+  print(): void {
+    console.log("---- Member 1 ----");
+    console.log(`ID: ${this.id}`);
+    console.log(`Name: ${this.name}`);
+    console.log(`Role: ${roleToString(this.role)}`);
+    console.log(`Age: ${this.age}`);
+    console.log(`Email: ${this.email}`);
+    console.log(`Active: ${this.isActive}`);
+  },
+};
+
+member1Object.print();
